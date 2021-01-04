9ea02af452dfb06bb07ba176904e5822c07de792
nitish sharma <nitish.sharma@plutonic.co.in>
2021-01-04 23:05:54 +0530

    install antd library

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link, BrowserRouter as Router,} from "react-router-dom";
+import {Button} from "antd";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        <Router>
+        <Link to={'https://reactjs.org'}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+        <Button type={'primary'}>  Learn React</Button>
+        </Link>
+        </Router>
       </header>
     </div>
   );
